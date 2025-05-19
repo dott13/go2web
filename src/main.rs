376928b1051dd04875aa77defcc127968dc4a34e
dott13 <supreme.notfound@gmail.com>
2025-05-20 00:02:39 +0300

    added raw TCP HTTP GET for -u option

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,72 @@
 use clap::{Arg, Command};
+use std::{io::{Read, Write}, net::TcpStream};
 
 fn main() {
     let matches = Command::new("go2web")
-        .version("0.1")
+        .version("0.2")
         .author("dott13")
         .about("Make raw HTTP requests or search via CLI")
         .arg(
             Arg::new("url")
                 .short('u')
                 .long("url")
-                .takes_value(true)
+                .num_args(1)
                 .help("Make an HTTP GET request to a URL"),
         )
         .arg(
             Arg::new("search")
                 .short('s')
                 .long("search")
-                .takes_value(true)
+                .num_args(1)
                 .help("Search the term using a search engine"),
-        )
-        .arg(
-            Arg::new("help")
-                .short('h')
-                .long("help")
-                .action(clap::ArgAction::Help),
         )
         .get_matches();
 
     if let Some(url) = matches.get_one::<String>("url") {
-        println!("You selected URL: {}", url);
+        handle_http_request(url);
     } else if let Some(term) = matches.get_one::<String>("search") {
         println!("You selected search: {}", term);
+    }
+
+    fn handle_http_request(url: &str) {
+        let (host, path) = match parse_url(url) {
+            Some(parts) => parts,
+            None => {
+                eprintln!("Invalid URL format. Use http://host/path");
+                return;
+            }
+        };
+
+        let addr = format!("{}:80", host);
+        let mut stream = TcpStream::connect(&addr).expect("Failed to connect to server");
+
+        let request = format!(
+            "GET {} HTTP/1.1\r\nHost: {}\r\nConnection: close\r\n\r\n",
+            path, host
+        );
+
+        stream
+            .write_all(request.as_bytes())
+            .expect("Failed to send request");
+
+        let mut response = String::new();
+        stream
+            .read_to_string(&mut response)
+            .expect(" ----- BAD RESPONSE -----");
+
+        println!(" ----- RAW RESPONSE -----");
+        println!("{}", response);
+    }
+
+    fn parse_url(url: &str) -> Option<(String, String)>{
+        if !url.starts_with("http://") {
+            return None;
+        }
+
+        let without_scheme = &url[7..];
+        let mut parts = without_scheme.splitn(2, '/');
+        let host = parts.next()?.to_string();
+        let path = format!("/{}", parts.next().unwrap_or(""));
+        Some((host, path))
     }
 }
